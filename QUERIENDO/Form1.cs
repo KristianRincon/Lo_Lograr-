ee76ddbc972fb386ae4bdda94f01539e64304f09
KRISTIAN DANILO RINCON <kdrincong@unadvirtual.edu.co>
2022-01-29 20:40:41 -0500

    Se dejo la instruccion anterior del metodo btnSI_Click y ademas se agregaron algunos comentarios al codigo.

--- QUERIENDO/Form1.cs
+++ QUERIENDO/Form1.cs
@@ -19,23 +19,25 @@ namespace QUERIENDO
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            label1.Parent = pictureBox1;
-            label2.Parent = pictureBox2;
-            panel2.Hide();
-            btnSI.TabStop = false;
+            label1.Parent = pictureBox1; //se configura el pictureBox1 como el control padre de la etiqueta para que se aplique
+                                         //la propiedad transparente a la etiqueta "label1"
+            label2.Parent = pictureBox2; //igual a la anterior
+            panel2.Hide();               //el metodo .Hide() oculta el primer formulario
+            btnSI.TabStop = false;       // es para que no se puede utilizar el Tabulador con los botones   
             btnNO.TabStop = false;
         }
         public void MoverBoton()
         {
-            Random R = new Random();
-            int x = R.Next(0, this.Width - btnNO.Width);
-            int y = R.Next(0, this.Height - btnNO.Height);
-            btnNO.Location = new Point(x, y);
+            Random R = new Random(); //con el metodo random creamos numeros aleatorios
+            int x = R.Next(0, this.Width - btnNO.Width); //se ajustan la propiedades del boton NO para que cambie la ubicacion
+                                                         //a un lugar aleatorio el el ancho del panel
+            int y = R.Next(0, this.Height - btnNO.Height); //igual al anterior pero con el alto del panel
+            btnNO.Location = new Point(x, y); //Inicializa una nueva instancia de la estructura Point con las coordenadas especificadas.
         }
 
-        private void btnNO_MouseMove(object sender, MouseEventArgs e)
-        {
-            MoverBoton();
+        private void btnNO_MouseMove(object sender, MouseEventArgs e)//Object Sender es un parámetro llamado Sender que contiene una referencia al control / objeto que provoca el evento.
+        {                                                            //btnNO_MouseMove ocurre cuando el puntero del mouse se mueve sobre el boton
+            MoverBoton();//se ejecuta este metodo
             if (btnNO.Location == btnSI.Location || btnNO.Location == label1.Location)
             {// si el boton NO se situa sobre el boton SI o el boton NO se situa sobre el texto "Quieres..." se ejecuta el metodo MoverBoton
                 MoverBoton();
@@ -44,12 +46,13 @@ namespace QUERIENDO
 
         private void btnSI_Click(object sender, EventArgs e)
         {
-            // panel2.Show();
-            MoverBoton();
+           // se muestra el segundo panel que hemos creado cuando hacemos click sobre el boton SI
+            panel2.Show();           
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //se cierra la aplicacion con el boton1 "Boton de aceptar"
             Application.Exit();
         }
     }
